f5f42594f3057400c810d1b87d270ba247a7f732
Miguel Palhas <mpalhas@gmail.com>
2016-03-16 23:30:37 +0000

    Fixes background gradient

---
+++ Assets/Scripts/Game/screenScaler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenScaler : MonoBehaviour {
+
+  void Start() {
+    var y = Camera.main.orthographicSize * 2.0f;
+    var x = y * Screen.width / Screen.height;
+    transform.localScale = new Vector3(x, y, 1);
+  }
+}
